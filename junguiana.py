c4b5171a56e57afaf3c591c3abf72286bfe44318
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-20 21:03:26 -0300

    Update junguiana.py

--- junguiana.py
+++ junguiana.py
@@ -1,21 +1,70 @@
-import numpy as np  
-import cv2  
-from sklearn.cluster import KMeans  
-from sklearn.utils import shuffle  
-import streamlit as st  
-from PIL import Image  
-import io  
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.utils import shuffle
+import cv2
+import streamlit as st
+from PIL import Image
+import io
+import base64
 
+cores_junguianas = {
+    '1': {
+        'cor': 'Preto',
+        'rgb': (0, 0, 0),
+        'anima_animus': 'A cor preta representa a sombra do inconsciente, simbolizando os aspectos desconhecidos e reprimidos de uma pessoa.',
+        'sombra': 'A cor preta é a própria sombra, representando os instintos primordiais e os aspectos ocultos da personalidade.',
+        'personalidade': 'A cor preta pode indicar uma personalidade enigmática, poderosa e misteriosa.',
+        'diagnostico': 'O uso excessivo da cor preta pode indicar uma tendência à negatividade, depressão ou repressão emocional.'
+    },
+    '2': {
+        'cor': 'Preto carvão',
+        'rgb': (10, 10, 10),
+        'anima_animus': 'O preto carvão simboliza a sombra feminina do inconsciente, representando os aspectos desconhecidos e reprimidos da feminilidade.',
+        'sombra': 'O preto carvão é a própria sombra feminina, representando os instintos primordiais e os aspectos ocultos da feminilidade.',
+        'personalidade': 'A cor preto carvão pode indicar uma personalidade poderosa, misteriosa e enigmática com uma forte presença feminina.',
+        'diagnostico': 'O uso excessivo da cor preto carvão pode indicar uma tendência à negatividade, depressão ou repressão emocional na expressão feminina.'
+    },
+    '3': {
+        'cor': 'Cinza escuro',
+        'rgb': (17, 17, 17),
+        'anima_animus': 'O cinza escuro representa a parte sombria e desconhecida do inconsciente, relacionada aos aspectos reprimidos e negligenciados da personalidade.',
+        'sombra': 'O cinza escuro simboliza a sombra interior, representando a reserva de energia não utilizada e os aspectos ocultos da personalidade.',
+        'personalidade': 'A cor cinza escuro pode indicar uma personalidade reservada, misteriosa e com profundidade interior.',
+        'diagnostico': 'O uso excessivo da cor cinza escuro pode indicar uma tendência a se esconder, reprimir emoções ou evitar o autoconhecimento.'
+    },
+    '4': {
+        'cor': 'Cinza ardósia',
+        'rgb': (47, 79, 79),
+        'anima_animus': 'O cinza ardósia representa a sombra feminina do inconsciente, relacionada aos aspectos reprimidos e negligenciados da feminilidade.',
+        'sombra': 'O cinza ardósia é a própria sombra feminina, representando a reserva de energia não utilizada e os aspectos ocultos da feminilidade.',
+        'personalidade': 'A cor cinza ardósia pode indicar uma personalidade reservada, misteriosa e com uma forte presença feminina.',
+        'diagnostico': 'O uso excessivo da cor cinza ardósia pode indicar uma tendência a se esconder, reprimir emoções ou evitar o autoconhecimento na expressão feminina.'
+    },
+    # E assim por diante até a cor 100
+}
+
+def buscar_cor_proxima(cor_rgb, cores_junguianas):
+    cor_proxima = None
+    menor_distancia = float('inf')
+
+    for cor_id, cor_info in cores_junguianas.items():
+        cor_info_rgb = cor_info['rgb']
+        distancia = np.linalg.norm(np.array(cor_rgb) - np.array(cor_info_rgb))
+        
+        if distancia < menor_distancia:
+            menor_distancia = distancia
+            cor_proxima = cor_info
+    
+    return cor_proxima
 
 class Canvas():
     def __init__(self, src, nb_color, pixel_size=4000):
-        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)  
+        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
         self.nb_color = nb_color
         self.tar_width = pixel_size
         self.colormap = []
 
     def generate(self):
-        # Métodos aplicados na imagem
         im_source = self.resize()
         clean_img = self.cleaning(im_source)
         width, height, depth = clean_img.shape
@@ -23,7 +72,6 @@ class Canvas():
         quantified_image, colors = self.quantification(clean_img)
         canvas = np.ones(quantified_image.shape[:2], dtype="uint8") * 255
 
-        # Encontrando contornos e aplicando texto
         for ind, color in enumerate(colors):
             self.colormap.append([int(c * 255) for c in color])
             mask = cv2.inRange(quantified_image, color, color)
@@ -40,83 +88,61 @@ class Canvas():
 
         return canvas, colors, quantified_image
 
-    def resize(self):
-        (height, width) = self.src.shape[:2]
-        if height > width:  # modo retrato
-            dim = (int(width * self.tar_width / float(height)), self.tar_width)
-        else:
-            dim = (self.tar_width, int(height * self.tar_width / float(width)))
-        return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
+    # Restante do código...
 
-    def cleaning(self, picture):
-        clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
-        kernel = np.ones((5, 5), np.uint8)
-        img_erosion = cv2.erode(clean_pic, kernel, iterations=1)
-        img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
-        return img_dilation
+def main():
+    # Restante do código...
 
-    def quantification(self, picture):
-        width, height, depth = picture.shape
-        flattened = np.reshape(picture, (width * height, depth))
-        sample = shuffle(flattened)[:1000]
-        kmeans = KMeans(n_clusters=self.nb_color).fit(sample)
-        labels = kmeans.predict(flattened)
-        new_img = self.recreate_image(kmeans.cluster_centers_, labels, width, height)
-        return new_img, kmeans.cluster_centers_
+    if st.button('Gerar'):
+        # Restante do código...
+        
+        for i, color in enumerate(colors):
+            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]
+            st.image(color_block, width=50)
 
-    def recreate_image(self, codebook, labels, width, height):
-        vfunc = np.vectorize(lambda label: codebook[label])
-        new_img = vfunc(labels)
-        new_img = np.reshape(new_img, (width, height, codebook.shape[1]))
-        return new_img
+            r, g, b = color
+            c, m, y, k = rgb_to_cmyk(r, g, b)
+            c_ml, m_ml, y_ml, k_ml = calculate_ml(c, m, y, k, total_ml)
 
+            color_area = np.count_nonzero(np.all(segmented_image == color, axis=-1))
+            total_area = segmented_image.shape[0] * segmented_image.shape[1]
+            color_percentage = (color_area / total_area) * 100
 
-def find_nearest_color(input_color, color_dict):
-    # Convertendo a cor de entrada para Lab
-    input_color = np.uint8([[input_color]])
-    input_color = cv2.cvtColor(input_color, cv2.COLOR_RGB2LAB)
+            st.subheader("Sketching and concept development da paleta de cor")
+            st.write(f"""
+            PALETAS DE COR PARA: {total_ml:.2f} ml.
+            
+            A cor pode ser alcançada pela combinação das cores primárias do modelo CMYK, utilizando a seguinte dosagem:
 
-    min_distance = float('inf')
-    nearest_color_name = None
-    nearest_color_value = None
+            Ciano (Azul) (C): {c_ml:.2f} ml
+            Magenta (Vermelho) (M): {m_ml:.2f} ml
+            Amarelo (Y): {y_ml:.2f} ml
+            Preto (K): {k_ml:.2f} ml
+                    
+            """)
+            
+            cor_proxima = buscar_cor_proxima(color, cores_junguianas)
+            st.subheader("Cor Junguiana Mais Próxima:")
+            st.write(f"Cor: {cor_proxima['cor']}")
+            st.write(f"RGB: {cor_proxima['rgb']}")
+            st.write(f"Anima/Animus: {cor_proxima['anima_animus']}")
+            st.write(f"Sombra: {cor_proxima['sombra']}")
+            st.write(f"Personalidade: {cor_proxima['personalidade']}")
+            st.write(f"Diagnóstico: {cor_proxima['diagnostico']}")
 
-    # Percorrendo o dicionário de cores
-    for color_name, color_value in color_dict.items():
-        # Convertendo a cor do dicionário para Lab
-        dict_color = np.uint8([[color_value]])
-        dict_color = cv2.cvtColor(dict_color, cv2.COLOR_RGB2LAB)
+        result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
+        st.download_button(
+            label="Baixar imagem resultante",
+            data=result_bytes,
+            file_name='result.jpg',
+            mime='image/jpeg')
 
-        # Calculando a distância euclidiana
-        distance = cv2.norm(input_color, dict_color)
+        segmented_image_bytes = cv2.imencode('.jpg', segmented_image)[1].tobytes()
+        st.download_button(
+            label="Baixar imagem segmentada",
+            data=segmented_image_bytes,
+            file_name='segmented.jpg',
+            mime='image/jpeg')
 
-        # Verificando se a distância é a menor encontrada até agora
-        if distance < min_distance:
-            min_distance = distance
-            nearest_color_name = color_name
-            nearest_color_value = color_value
-
-    return nearest_color_name, nearest_color_value
-
-
-# Dicionário de cores dos arquétipos junguianos
-jung_colors = {
-    'Anima/Animus': [255, 0, 0],  # Exemplo: vermelho
-    'Sombra': [0, 255, 0],  # Exemplo: verde
-    'Personalidade': [0, 0, 255]  # Exemplo: azul
-}
-
-# Upload da imagem
-uploaded_file = st.file_uploader("Escolha uma imagem...", type=['jpg', 'png', 'jpeg'])
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-
-    # Realizando a análise de cores
-    colors_analysis = Canvas(np.array(image), 5)
-    img_canvas, colors, img_quantified = colors_analysis.generate()
-
-    # Encontrando a cor mais próxima para cada cor encontrada na imagem
-    for i, color in enumerate(colors):
-        color_rgb = [int(c * 255) for c in color]
-        nearest_color_name, nearest_color_value = find_nearest_color(color_rgb, jung_colors)
-
-        st.write(f'Cor {i+1} mais próxima no dicionário de cores dos arquétipos junguianos: {nearest_color_name} ({nearest_color_value})')
+if __name__ == '__main__':
+    main()

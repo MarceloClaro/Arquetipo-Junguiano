f937d0b3692de36f3f53861c531913e1bf358ece
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-21 21:19:14 -0300

    Update junguiana.py

--- junguiana.py
+++ junguiana.py
@@ -8,63 +8,133 @@ from PIL import Image
 import io
 import base64
 
-# Aqui estamos criando uma nova ferramenta que chamamos de "Canvas".
-# Isso nos ajuda a lidar com imagens e cores.
-#... 
-# As funções e a classe Canvas vão aqui
-#...
+# Funções auxiliares para conversão de cores
+def rgb_to_cmyk(r, g, b):
+    if (r == 0) and (g == 0) and (b == 0):
+        return 0, 0, 0, 1
+    c = 1 - r / 255
+    m = 1 - g / 255
+    y = 1 - b / 255
 
-st.image("clube.png")  # Adiciona a imagem no topo do app
-st.title('Gerador de Paleta de Cores para Pintura por Números ')
-st.subheader("""
-Autor: Marcelo Claro
+    min_cmy = min(c, m, y)
+    c = (c - min_cmy) / (1 - min_cmy)
+    m = (m - min_cmy) / (1 - min_cmy)
+    y = (y - min_cmy) / (1 - min_cmy)
+    k = min_cmy
 
-https://orcid.org/0000-0001-8996-2887
+    return c, m, y, k
 
-marceloclaro@geomaker.org
+def calculate_ml(c, m, y, k, total_ml):
+    total_ink = c + m + y + k
+    c_ml = (c / total_ink) * total_ml
+    m_ml = (m / total_ink) * total_ml
+    y_ml = (y / total_ink) * total_ml
+    k_ml = (k / total_ink) * total_ml
+    return c_ml, m_ml, y_ml, k_ml
 
-Whatsapp:(88)98158-7145 (https://www.geomaker.org/)
-""")
+# Classe Canvas para processar a imagem e realizar as operações
+class Canvas():
+    def __init__(self, src, nb_color, pixel_size=4000):
+        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+        self.nb_color = nb_color
+        self.tar_width = pixel_size
+        self.colormap = []
 
-# Isso é para as pessoas fazerem o upload de uma imagem que elas querem usar.
-uploaded_file = st.file_uploader("1. Carregue uma imagem", type=["jpg", "png"])
+    def generate(self):
+        im_source = self.resize()
+        clean_img = self.cleaning(im_source)
+        width, height, depth = clean_img.shape
+        clean_img = np.array(clean_img, dtype="uint8") / 255
+        quantified_image, colors = self.quantification(clean_img)
+        canvas = np.ones(quantified_image.shape[:2], dtype="uint8") * 255
 
+        for ind, color in enumerate(colors):
+            self.colormap.append([int(c * 255) for c in color])
+            mask = cv2.inRange(quantified_image, color, color)
+            cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+            for contour in cnts:
+                _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
+                if width_ctr > 10 and height_ctr > 10 and cv2.contourArea(contour, True) < -100:
+                    cv2.drawContours(canvas, [contour], -1, (0, 0, 0), 1)
+                    txt_x, txt_y = contour[0][0]
+                    cv2.putText(canvas, '{:d}'.format(ind + 1), (txt_x, txt_y + 15),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
+        return canvas, colors, quantified_image
+
+    def resize(self):
+        (height, width) = self.src.shape[:2]
+        if height > width:
+            dim = (int(width * self.tar_width / float(height)), self.tar_width)
+        else:
+            dim = (self.tar_width, int(height * self.tar_width / float(width)))
+        return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
+
+    def cleaning(self, picture):
+        clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
+        kernel = np.ones((5, 5), np.uint8)
+        img_erosion = cv2.erode(clean_pic, kernel, iterations=1)
+        img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+        return img_dilation
+
+    def quantification(self, picture):
+        width, height, depth = picture.shape
+        flattened = np.reshape(picture, (width * height, depth))
+        sample = shuffle(flattened)[:1000]
+        kmeans = KMeans(n_clusters=self.nb_color).fit(sample)
+        labels = kmeans.predict(flattened)
+        new_img = self.recreate_image(kmeans.cluster_centers_, labels, width, height)
+        return new_img, kmeans.cluster_centers_
+
+    def recreate_image(self, codebook, labels, width, height):
+        vfunc = lambda x: codebook[labels[x]]
+        out = vfunc(np.arange(width * height))
+        return np.resize(out, (width, height, codebook.shape[1]))
+
+# Configuração inicial do app
+st.image("clube.png")
+st.title('Gerador de Pintura por Números')
+st.subheader("Sketching and concept development")
+
+# Carregar imagem
+uploaded_file = st.file_uploader("Carregue uma imagem", type=["jpg", "png"])
+st.write("Bem-vindo ao Gerador de Pintura por Números!")
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Corrige a ordem dos canais de cor
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     st.image(image, caption='Imagem Carregada', use_column_width=True)
 
-    nb_color = st.slider('2. Escolha o número de cores para pintar', min_value=1, max_value=80, value=2, step=1)
-    pixel_size = st.slider('Escolha o tamanho do pixel da pintura', min_value=500, max_value=8000, value=4000, step=100)
+    # Escolher o número de clusters
+    nb_color = st.slider('Escolha o número de clusters', min_value=2, max_value=20, value=4, step=1)
 
-    if st.button('3. Clique em "GERAR TELA"'):
-        canvas = Canvas(image, nb_color, pixel_size)
+    if st.button('Gerar Tela'):
+        canvas = Canvas(image, nb_color)
         result, colors, segmented_image = canvas.generate()
 
         # Converter imagem segmentada para np.uint8
         segmented_image = (segmented_image * 255).astype(np.uint8)
-        # Agora converta de BGR para RGB
+        
+        # Converter de BGR para RGB
         segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
 
         st.image(result, caption='Imagem Resultante', use_column_width=True)
         st.image(segmented_image, caption='Imagem Segmentada', use_column_width=True)
 
-        # Mostrar paleta de cores
-        for i, color in enumerate(colors):
-            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]  # Cores em formato BGR
-            st.image(color_block, caption=f'Cor {i+1}', width=50)
-
+        # Salvar contorno e paleta
         result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
         st.download_button(
-            label="4. Salve o contorno",
+            label="Baixar Contorno",
             data=result_bytes,
-            file_name='result.jpg',
+            file_name='contorno.jpg',
             mime='image/jpeg')
 
-        segmented_image_bytes = cv2.imencode('.jpg', segmented_image)[1].tobytes()
+        palette_bytes = cv2.imencode('.jpg', segmented_image)[1].tobytes()
         st.download_button(
-            label="Salve a imagem segmentada",
-            data=segmented_image_bytes,
-            file_name='segmented.jpg',
+            label="Baixar Paleta",
+            data=palette_bytes,
+            file_name='paleta.jpg',
             mime='image/jpeg')
+

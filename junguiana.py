8dd4fa3a5ea948243c580483f78350bc380125b0
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-21 21:11:26 -0300

    Update junguiana.py

--- junguiana.py
+++ junguiana.py
@@ -1,3 +1,4 @@
+# Importando todas as coisas necessárias para o nosso programa funcionar.
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
@@ -7,11 +8,14 @@ from PIL import Image
 import io
 import base64
 
-# Supondo que a função rgb_to_cmyk, calculate_ml e a classe Canvas estão definidas.
+# Aqui estamos criando uma nova ferramenta que chamamos de "Canvas".
+# Isso nos ajuda a lidar com imagens e cores.
+#... 
+# As funções e a classe Canvas vão aqui
+#...
 
-st.image("clube.png")
-st.title('Gerador de Paleta de Cores para Pintura por Números')
-st.subheader("Sketching and concept development")
+st.image("clube.png")  # Adiciona a imagem no topo do app
+st.title('Gerador de Paleta de Cores para Pintura por Números ')
 st.subheader("""
 Autor: Marcelo Claro
 
@@ -22,40 +26,40 @@ marceloclaro@geomaker.org
 Whatsapp:(88)98158-7145 (https://www.geomaker.org/)
 """)
 
+# Isso é para as pessoas fazerem o upload de uma imagem que elas querem usar.
 uploaded_file = st.file_uploader("1. Carregue uma imagem", type=["jpg", "png"])
 
 if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     image = cv2.imdecode(file_bytes, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Corrige a ordem dos canais de cor
     st.image(image, caption='Imagem Carregada', use_column_width=True)
 
     nb_color = st.slider('2. Escolha o número de cores para pintar', min_value=1, max_value=80, value=2, step=1)
-    total_ml = st.slider('Escolha o total em ml da tinta de cada cor', min_value=1, max_value=1000, value=10, step=1)
     pixel_size = st.slider('Escolha o tamanho do pixel da pintura', min_value=500, max_value=8000, value=4000, step=100)
 
-    if st.button('3. Gerar Tela'):
-        # Aqui incluiríamos o código para gerar a tela usando o KMeans e outras funcionalidades
+    if st.button('3. Clique em "GERAR TELA"'):
+        canvas = Canvas(image, nb_color, pixel_size)
+        result, colors, segmented_image = canvas.generate()
 
-        # Suponha que "result" é a imagem resultante e "segmented_image" é a imagem segmentada
+        # Converter imagem segmentada para np.uint8
+        segmented_image = (segmented_image * 255).astype(np.uint8)
+        # Agora converta de BGR para RGB
+        segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
+
         st.image(result, caption='Imagem Resultante', use_column_width=True)
         st.image(segmented_image, caption='Imagem Segmentada', use_column_width=True)
 
-        if st.button('4. Salvar contorno e paleta'):
-            # Fornecer botões para baixar a imagem resultante e a imagem segmentada
-            result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
-            st.download_button(
-                label="Baixar imagem resultante",
-                data=result_bytes,
-                file_name='result.jpg',
-                mime='image/jpeg')
+        # Mostrar paleta de cores
+        for i, color in enumerate(colors):
+            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]  # Cores em formato BGR
+            st.image(color_block, caption=f'Cor {i+1}', width=50)
 
-            segmented_image_bytes = cv2.imencode('.jpg', segmented_image)[1].tobytes()
-            st.download_button(
-                label="Baixar imagem segmentada",
-                data=segmented_image_bytes,
-                file_name='segmented.jpg',
-                mime='image/jpeg')
+        result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
+        st.download_button(
+            label="4. Salve o contorno",
+            data=result_bytes,
+            file_name='result.jpg',
+            mime='image/jpeg')
 
-else:
-    st.error("Por favor, carregue uma imagem.")  # Mensagem de erro caso nenhuma imagem seja carregada
+        segmented_image_bytes = cv2.imencode('.jpg', segmented_image

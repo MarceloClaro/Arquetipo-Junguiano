3b496f27ed6864f5f05502b16c137d873ca7551f
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-06-12 06:11:32 -0300

    Update junguiana.py

--- junguiana.py
+++ junguiana.py
@@ -1,218 +1,89 @@
-import numpy as np
-from sklearn.cluster import KMeans
-from sklearn.utils import shuffle
-import cv2
-from PIL import Image
-import io
-import base64
 import streamlit as st
+import numpy as np
+from PIL import Image
+from sklearn.cluster import KMeans
+from scipy.spatial import distance
 
+# Função para encontrar a cor mais próxima no dicionário
+def encontrar_cor_mais_proxima(rgb):
+    cor_mais_proxima = None
+    menor_distancia = float('inf')
+
+    for chave, valor in cores_junguianas.items():
+        distancia_cor = distance.euclidean(rgb, valor['rgb'])
+        if distancia_cor < menor_distancia:
+            menor_distancia = distancia_cor
+            cor_mais_proxima = valor
+
+    return cor_mais_proxima
+
+# Carregar o dicionário com as cores junguianas
 cores_junguianas = {
-    '1': {
-        'cor': 'Preto',
-        'rgb': (0, 0, 0),
-        'anima_animus': 'A cor preta representa a sombra do inconsciente, simbolizando os aspectos desconhecidos e reprimidos de uma pessoa.',
-        'sombra': 'A cor preta é a própria sombra, representando os instintos primordiais e os aspectos ocultos da personalidade.',
-        'personalidade': 'A cor preta pode indicar uma personalidade enigmática, poderosa e misteriosa.',
-        'diagnostico': 'O uso excessivo da cor preta pode indicar uma tendência à negatividade, depressão ou repressão emocional.'
-    },
-    '2': {
-        'cor': 'Preto carvão',
-        'rgb': (10, 10, 10),
-        'anima_animus': 'O preto carvão simboliza a sombra feminina do inconsciente, representando os aspectos desconhecidos e reprimidos da feminilidade.',
-        'sombra': 'O preto carvão é a própria sombra feminina, representando os instintos primordiais e os aspectos ocultos da feminilidade.',
-        'personalidade': 'A cor preto carvão pode indicar uma personalidade poderosa, misteriosa e enigmática com uma forte presença feminina.',
-        'diagnostico': 'O uso excessivo da cor preto carvão pode indicar uma tendência à negatividade, depressão ou repressão emocional na expressão feminina.'
-    },
-    '3': {
-        'cor': 'Cinza escuro',
-        'rgb': (17, 17, 17),
-        'anima_animus': 'O cinza escuro representa a parte sombria e desconhecida do inconsciente, relacionada aos aspectos reprimidos e negligenciados da personalidade.',
-        'sombra': 'O cinza escuro simboliza a sombra interior, representando a reserva de energia não utilizada e os aspectos ocultos da personalidade.',
-        'personalidade': 'A cor cinza escuro pode indicar uma personalidade reservada, misteriosa e com profundidade interior.',
-        'diagnostico': 'O uso excessivo da cor cinza escuro pode indicar uma tendência a se esconder, reprimir emoções ou evitar o autoconhecimento.'
-    },
-    '4': {
-        'cor': 'Cinza ardósia',
-        'rgb': (47, 79, 79),
-        'anima_animus': 'O cinza ardósia representa a sombra feminina do inconsciente, relacionada aos aspectos reprimidos e negligenciados da feminilidade.',
-        'sombra': 'O cinza ardósia é a própria sombra feminina, representando a reserva de energia não utilizada e os aspectos ocultos da feminilidade.',
-        'personalidade': 'A cor cinza ardósia pode indicar uma personalidade reservada, misteriosa e com uma forte presença feminina.',
-        'diagnostico': 'O uso excessivo da cor cinza ardósia pode indicar uma tendência a se esconder, reprimir emoções ou evitar o autoconhecimento na expressão feminina.'
-    },
+    # Seu dicionário de cores aqui...
 }
 
-def rgb_to_cmyk(r, g, b):
-    if r == 0 and g == 0 and b == 0:
-        return 0, 0, 0, 1
+# Configurações do aplicativo Streamlit
+st.image("clube.png")  # Adiciona a imagem no topo do app
+st.title("Análise de Cor Dominante das Pinturas de Telas")
+st.write("""
+Este aplicativo é usado para analisar a cor dominante em uma pintura e encontrar sua correspondência em um dicionário de cores com significados.
 
-    c = 1 - r / 255
-    m = 1 - g / 255
-    y = 1 - b / 255
-    min_cmy = min(c, m, y)
-    c, m, y = (c - min_cmy) / (1 - min_cmy), (m - min_cmy) / (1 - min_cmy), (y - min_cmy) / (1 - min_cmy)
-    k = min_cmy
-
-    return c, m, y, k
-
-def calculate_ml(c, m, y, k, total_ml):
-    total_ink = c + m + y + k
-    ratio = total_ml / total_ink
-    c_ml, m_ml, y_ml, k_ml = c * ratio, m * ratio, y * ratio, k * ratio
-    return c_ml, m_ml, y_ml, k_ml
-
-def buscar_cor_proxima(rgb, cores_junguianas):
-    distancias = [np.sqrt(np.sum((np.array(rgb) - np.array(cor_junguiana['rgb'])) ** 2))
-                  for cor_junguiana in cores_junguianas.values()]
-    cor_proxima_index = np.argmin(distancias)
-    return cores_junguianas[str(cor_proxima_index + 1)]
-
-class Canvas():
-    def __init__(self, src, nb_color, pixel_size=4000):
-        self.src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-        self.nb_color = nb_color
-        self.tar_width = pixel_size
-        self.colormap = []
-
-    def generate(self):
-        im_source = self.resize()
-        clean_img = self.cleaning(im_source)
-        width, height, depth = clean_img.shape
-        clean_img = np.array(clean_img, dtype="uint8") / 255
-        quantified_image, colors = self.quantification(clean_img)
-        canvas = np.ones(quantified_image.shape[:2], dtype="uint8") * 255
-
-        for ind, color in enumerate(colors):
-            self.colormap.append([int(c * 255) for c in color])
-            mask = cv2.inRange(quantified_image, color, color)
-            cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-            for contour in cnts:
-                _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
-                if width_ctr > 10 and height_ctr > 10 and cv2.contourArea(contour, True) < -100:
-                    cv2.drawContours(canvas, [contour], -1, (0, 0, 0), 1)
-                    txt_x, txt_y = contour[0][0]
-                    cv2.putText(canvas, '{:d}'.format(ind + 1), (txt_x, txt_y + 15),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
-
-        return canvas, colors, quantified_image
-
-    def resize(self):
-        (height, width) = self.src.shape[:2]
-        if height > width:
-            dim = (int(width * self.tar_width / float(height)), self.tar_width)
-        else:
-            dim = (self.tar_width, int(height * self.tar_width / float(width)))
-        return cv2.resize(self.src, dim, interpolation=cv2.INTER_AREA)
-
-    def cleaning(self, picture):
-        clean_pic = cv2.fastNlMeansDenoisingColored(picture, None, 10, 10, 7, 21)
-        kernel = np.ones((5, 5), np.uint8)
-        img_erosion = cv2.erode(clean_pic, kernel, iterations=1)
-        img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
-        return img_dilation
-
-    def quantification(self, picture):
-        width, height, depth = picture.shape
-        flattened = np.reshape(picture, (width * height, depth))
-        sample = shuffle(flattened)[:1000]
-        kmeans = KMeans(n_clusters=self.nb_color).fit(sample)
-        labels = kmeans.predict(flattened)
-        new_img = self.recreate_image(kmeans.cluster_centers_, labels, width, height)
-        return new_img, kmeans.cluster_centers_
-
-    def recreate_image(self, codebook, labels, width, height):
-        vfunc = np.vectorize(lambda x: codebook[labels[x]])
-        out = vfunc(np.arange(width * height))
-        return np.resize(out, (width, height, codebook.shape[1]))
-
-st.image("clube.png")
-st.title('Gerador de Paleta de Cores para Pintura por Números')
-st.subheader("Sketching and concept development")
-st.subheader("""
-Autor: Marcelo Claro
-
-https://orcid.org/0000-0001-8996-2887
-
-marceloclaro@geomaker.org
-
-Whatsapp:(88)98158-7145 (https://www.geomaker.org/)
+Em resumo, este aplicativo pode ajudar a revelar os significados ocultos nas cores usadas em obras de arte.
 """)
-st.write("Apresento a vocês um aplicativo chamado 'Gerador de Paleta de Cores para Pintura por Números'. Esse aplicativo existência.")
+st.write("Carregue uma imagem e descubra a cor dominante e sua correspondência no dicionário de cores junguianas.")
 
-uploaded_file = st.file_uploader("Escolha uma imagem", type=["jpg", "png"])
+# Carregar a imagem
+imagem = st.file_uploader("Selecione uma imagem", type=['jpg', 'jpeg', 'png'])
 
-if uploaded_file is not None:
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    st.image(image, caption='Imagem Carregada', use_column_width=True)
+if imagem is not None:
+    # Exibir a imagem carregada
+    imagem_pil = Image.open(imagem)
+    st.image(imagem_pil, caption="Imagem Original", use_column_width=True)
 
-    nb_color = st.slider('Escolha o número de cores para pintar', min_value=1, max_value=80, value=2, step=1)
-    total_ml = st.slider('Escolha o total em ml da tinta de cada cor', min_value=1, max_value=1000, value=10, step=1)
-    pixel_size = st.slider('Escolha o tamanho do pixel da pintura', min_value=500, max_value=8000, value=4000, step=100)
+    # Converter a imagem para um array numpy
+    imagem_array = np.array(imagem_pil)
 
-    if st.button('Gerar'):
-        pil_image = Image.open(io.BytesIO(file_bytes))
-        if 'dpi' in pil_image.info:
-            dpi = pil_image.info['dpi']
-            st.write(f'Resolução da imagem: {dpi} DPI')
+    # Redimensionar a imagem para 100x100 pixels
+    imagem_redimensionada = np.array(imagem_pil.resize((100, 100)))
 
-            cm_per_inch = pixel_size
-            cm_per_pixel = cm_per_inch / dpi[0]
-            st.write(f'Tamanho de cada pixel: {cm_per_pixel:.4f} centímetros')
+    # Obter os pixels da imagem redimensionada
+    pixels = imagem_redimensionada.reshape(-1, 3)
 
-        canvas = Canvas(image, nb_color, pixel_size)
-        result, colors, segmented_image = canvas.generate()
+    # Realizar a clusterização dos pixels
+    kmeans = KMeans(n_clusters=1)
+    kmeans.fit(pixels)
 
-        segmented_image = (segmented_image * 255).astype(np.uint8)
-        segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
+    # Encontrar a cor dominante
+    cor_dominante = kmeans.cluster_centers_[kmeans.labels_].mean(axis=0).astype(int)
 
-        st.image(result, caption='Imagem Resultante', use_column_width=True)
-        st.image(segmented_image, caption='Imagem Segmentada', use_column_width=True)
+    # Encontrar a cor mais próxima no dicionário
+    cor_proxima = encontrar_cor_mais_proxima(cor_dominante)
+    
+    # Exibir a cor dominante e sua correspondência no dicionário
+    st.subheader("Resultado")
+    st.write(f"Cor Dominante: RGB {cor_dominante}")
+    st.write(f"Cor Correspondente no Dicionário: {cor_proxima['cor']}")
+    st.write(f"Anima/Animico: {cor_proxima['anima_animico']}")
+    st.write(f"Sombra: {cor_proxima['sombra']}")
+    st.write(f"Personalidade: {cor_proxima['personalidade']}")
+    st.write(f"Diagnóstico: {cor_proxima['diagnostico']}")
+    
+    # Exibir a imagem segmentada do cluster
+    imagem_segmentada = cor_dominante.reshape(1, 1, 3)
+    st.image(imagem_segmentada, caption="Imagem Segmentada do Cluster", use_column_width=True)
 
-        for i, color in enumerate(colors):
-            color_block = np.ones((50, 50, 3), np.uint8) * color[::-1]
-            st.image(color_block, caption=f'Cor {i+1}', width=50)
+    # Variáveis do usuárioDesculpe-me pela interrupção, aqui está a continuação do código:
 
-            r, g, b = color
-            c, m, y, k = rgb_to_cmyk(r, g, b)
-            c_ml, m_ml, y_ml, k_ml = calculate_ml(c, m, y, k, total_ml)
+```python
+    # Variáveis do usuário
+    animico = st.slider("Anímico", 0.0, 100.0, (cor_proxima['anima_animico']))
+    sombra = st.slider("Sombra", 0.0, 100.0, (cor_proxima['sombra']))
+    personalidade = st.slider("Personalidade", 0.0, 100.0, (cor_proxima['personalidade']))
+    diagnostico = st.slider("Diagnóstico", 0.0, 100.0, (cor_proxima['diagnostico']))
 
-            color_area = np.count_nonzero(np.all(segmented_image == color, axis=-1))
-            total_area = segmented_image.shape[0] * segmented_image.shape[1]
-            color_percentage = (color_area / total_area) * 100
-
-            st.subheader("Sketching and concept development da paleta de cor")
-            st.write(f"""
-            PALETAS DE COR PARA: {total_ml:.2f} ml.
-
-            A cor pode ser alcançada pela combinação das cores primárias do modelo CMYK, utilizando a seguinte dosagem:
-
-            Ciano (Azul) (C): {c_ml:.2f} ml
-            Magenta (Vermelho) (M): {m_ml:.2f} ml
-            Amarelo (Y): {y_ml:.2f} ml
-            Preto (K): {k_ml:.2f} ml
-            """)
-
-            cor_proxima = buscar_cor_proxima(color, cores_junguianas)
-            st.write(f"      Cor Junguiana Mais Próxima: {cor_proxima['cor']}")
-            st.write(f"      Anima/Animus: {cor_proxima['anima_animus']}")
-            st.write(f"      Sombra: {cor_proxima['sombra']}")
-            st.write(f"      Personalidade: {cor_proxima['personalidade']}")
-            st.write(f"      Diagnóstico: {cor_proxima['diagnostico']}")
-
-        result_bytes = cv2.imencode('.jpg', result)[1].tobytes()
-        st.download_button(
-            label="Baixar imagem resultante",
-            data=result_bytes,
-            file_name='result.jpg',
-            mime='image/jpeg')
-
-        segmented_image_bytes = cv2.imencode('.jpg', segmented_image)[1].tobytes()
-        st.download_button(
-            label="Baixar imagem segmentada",
-            data=segmented_image_bytes,
-            file_name='segmented.jpg',
-            mime='image/jpeg')
+    # Exibir os resultados do usuário
+    st.subheader("Resultados do Usuário")
+    st.write(f"Anima/Animico: {animico}")
+    st.write(f"Sombra: {sombra}")
+    st.write(f"Personalidade: {personalidade}")
+    st.write(f"Diagnóstico: {diagnostico}")
